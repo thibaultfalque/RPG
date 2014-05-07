6fec4d1c3e1960e84fafa77a44d9142313e73e6a
thibault.falque <thibault.falque@gmail.com>
2014-05-08 00:40:14 +0200

    Ajout de la bibliothèque SFML compilé pour linux

---
+++ 01-Code/lib/SFML-2.1-linux/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/DependInfo.cmake
@@ -0,0 +1,63 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/CircleShape.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/CircleShape.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/Color.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Color.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/ConvexShape.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/ConvexShape.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/Font.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Font.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/GLCheck.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/GLCheck.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/Image.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Image.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/ImageLoader.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/ImageLoader.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/RectangleShape.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RectangleShape.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/RenderStates.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderStates.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/RenderTarget.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderTarget.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/RenderTexture.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderTexture.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/RenderTextureImpl.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderTextureImpl.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/RenderTextureImplDefault.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderTextureImplDefault.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/RenderTextureImplFBO.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderTextureImplFBO.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/RenderWindow.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/RenderWindow.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/Shader.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Shader.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/Shape.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Shape.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/Sprite.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Sprite.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/Text.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Text.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/Texture.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Texture.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/TextureSaver.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/TextureSaver.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/Transform.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Transform.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/Transformable.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Transformable.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/Vertex.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Vertex.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/VertexArray.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/VertexArray.cpp.o"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src/SFML/Graphics/View.cpp" "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/View.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "GLEW_STATIC"
+  "STBI_FAILURE_USERMSG"
+  )
+
+# Pairs of files generated by the same build rule.
+SET(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/home/thibault/Bibliotheque/C++/SFML2-build/lib/libsfml-graphics.so" "/home/thibault/Bibliotheque/C++/SFML2-build/lib/libsfml-graphics.so.2.1"
+  "/home/thibault/Bibliotheque/C++/SFML2-build/lib/libsfml-graphics.so.2" "/home/thibault/Bibliotheque/C++/SFML2-build/lib/libsfml-graphics.so.2.1"
+  )
+
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/Window/CMakeFiles/sfml-window.dir/DependInfo.cmake"
+  "/home/thibault/Bibliotheque/C++/SFML2-build/src/SFML/System/CMakeFiles/sfml-system.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/include"
+  "/home/thibault/Bibliotheque/C++/SFML-2.1/src"
+  "/usr/include/freetype2"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
